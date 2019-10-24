2c08ec8788506bcb57eb3337827f807dbe26bffc
Yang Jing <yang_ji@worksap.co.jp>
2019-10-24 20:39:49 +0800

    feat(proto): add .d.ts for the proto module

--- proto/index.js
+++ proto/index.js
@@ -1,1 +1,12 @@
-exports.Text = require('./gen/text_pb');
+let protos = [
+  require('./gen/text_pb'),
+  require('./gen/text_grpc_pb'),
+]
+
+for (let p of protos) {
+  for (let m in p) {
+    exports[m] = exports[m] || p[m];
+  }
+}
+
+exports.grpc = require('grpc');
